48585169689745e3ab261660afd31f4eac83a4ee
Paul Warren Seabrook <paulwarrenseabrook@gmail.com>
2023-02-16 15:19:11 -0500

    .env

--- src/utilities/movies-api.js
+++ src/utilities/movies-api.js
@@ -6,7 +6,7 @@ const BASE_URL = 'https://moviesdatabase.p.rapidapi.com/titles?limit=10&page=10'
 const options = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': 'e1cfa875a1msh2b63ee573dce486p158f9bjsnf99cf80dd680',
+    'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
     'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com',
   },
 }
