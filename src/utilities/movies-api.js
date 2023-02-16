ad3dcda5b2b7f7c0c68bea6c5ef9be1136d54ebe
Paul Warren Seabrook <paulwarrenseabrook@gmail.com>
2023-02-16 15:09:03 -0500

    working on pulling movies in

--- src/utilities/movies-api.js
+++ src/utilities/movies-api.js
@@ -1,1 +1,19 @@
+// import sendRequest from './send-request'
+
 // api requests for movies
+const BASE_URL = 'https://moviesdatabase.p.rapidapi.com/titles?limit=10&page=10'
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'e1cfa875a1msh2b63ee573dce486p158f9bjsnf99cf80dd680',
+    'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com',
+  },
+}
+
+export function getMovies() {
+  fetch(BASE_URL, options)
+    .then((response) => response.json())
+    .then((response) => console.log(response.results))
+    .catch((err) => console.error(err))
+}
