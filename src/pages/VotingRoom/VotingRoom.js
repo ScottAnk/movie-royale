695b033e66a8b85f6f382837c5a3c41d4f5e5ebe
eddie-hernandez <71785228+eddie-hernandez@users.noreply.github.com>
2023-02-16 22:29:33 -0500

    styling room pages ui

--- src/pages/VotingRoom/VotingRoom.js
+++ src/pages/VotingRoom/VotingRoom.js
@@ -1,12 +1,36 @@
 import { useNavigate } from 'react-router-dom'
+import MovieCard from '../../components/MovieCard/MovieCard'
+import './VotingRoom.css'
 
-export default function VotingRoom() {
+export default function VotingRoom({movies}) {
   return (
-    <div className="EnterRoom">
-      <h2>Let's Get Votin'</h2>
-      <ul>
-        <li>Movie 1</li>
-      </ul>
-    </div>
+    <div className="VotingRoom">
+        <div className="CardContainer">
+          <h2>Let's Get Votin'</h2>
+          <ul className="MovieCardContainer">
+            <li>Movie Test</li>
+            <li>Movie Test</li>
+            <li>Movie Test</li>
+            <li>Movie Test</li>
+            <li>Movie Test</li>
+            <li>Movie Test</li>
+            <li>Movie Test</li>
+            <li>Movie Test</li>
+            <li>Movie Test</li>
+            <li>Movie Test</li>
+            <li>Movie Test</li>
+            <li>Movie Test</li>
+            <li>Movie Test</li>
+            <li>Movie Test</li>
+            <li>Movie Test</li>
+            <li>Movie Test</li>
+            <li>Movie Test</li>
+            <li>Movie Test</li>
+            <li>Movie Test</li>
+            <li>Movie Test</li>
+          {/* <MovieCard movies={movies} /> */}
+          </ul>
+        </div>
+      </div>
   )
 }
