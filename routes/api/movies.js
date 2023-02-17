692b83902e12f7d47314fc048d917bd6d9b4af71
Paul Warren Seabrook <paulwarrenseabrook@gmail.com>
2023-02-17 17:54:39 -0500

    movies pull from back-end and show on ui

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 const movieCtrl = require('../../controllers/api/movies')
 
-
+// api/movies
 router.get('/', movieCtrl.getMovies)
 
 module.exports = router
--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,21 +1,13 @@
 export default function MovieCard({ movie }) {
   return (
-    <li style={{ listStyleType: 'none' }}>
-      <div
-        className="MovieCard"
-        // style={{
-        //   backgroundImage: `url('${movie.posterPath}')`,
-        //   backgroundPosition: 'center',
-        //   backgroundSize: 'contain',
-        //   width: '100%',
-        //   height: '400px',
-        //   backgroundRepeat: 'no-repeat',
-        // }}
-      >
-        <div className="movieInfo">
-          {/* <h2 style={{ backgroundColor: 'blue' }}>{movie.title}</h2>
-          <h3 style={{ backgroundColor: 'blue' }}>{movie.releaseDate}</h3> */}
-        </div>
+    <li
+      style={{
+        listStyleType: 'none',
+        backgroundImage: `url(${movie.thumbnail})`,
+      }}
+    >
+      <div className="MovieCard">
+        <div className="movieInfo">{movie.title}</div>
       </div>
     </li>
   )
