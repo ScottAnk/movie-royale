1dca9180cfc9e69c2f04c5e0520d62fc6faef5a6
eddie-hernandez <71785228+eddie-hernandez@users.noreply.github.com>
2023-02-18 21:23:50 -0500

    adding custom color palette

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -3,18 +3,9 @@ export default function MovieCard({ movie }) {
     <li style={{ listStyleType: 'none' }}>
       <div
         className="MovieCard"
-        // style={{
-        //   backgroundImage: `url('${movie.posterPath}')`,
-        //   backgroundPosition: 'center',
-        //   backgroundSize: 'contain',
-        //   width: '100%',
-        //   height: '400px',
-        //   backgroundRepeat: 'no-repeat',
-        // }}
+
       >
         <div className="movieInfo">
-          {/* <h2 style={{ backgroundColor: 'blue' }}>{movie.title}</h2>
-          <h3 style={{ backgroundColor: 'blue' }}>{movie.releaseDate}</h3> */}
         </div>
       </div>
     </li>
