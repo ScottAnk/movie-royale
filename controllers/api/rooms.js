f7f062790d8e22a8c6e342bb961b3f48ac0909a7
Scott Ankiewicz <scottfa.a@gmail.com>
2023-02-20 00:10:31 -0500

    move room code generation into Room schema

--- controllers/api/rooms.js
+++ controllers/api/rooms.js
@@ -3,12 +3,10 @@ const Room = require('../../models/room')
 
 // generate a new room in database and send it back as a response
 async function createRoom(req, res, next) {
-  const roomCode = await generateRoomCode()
   const roomName = `${req.user.name}'s Room`
 
   Room.create({
     ownerId: req.user._id,
-    roomCode: roomCode,
     roomName: roomName,
   })
     .then((room) => res.json(room))
@@ -64,39 +62,6 @@ async function recommend(req, res, next) {
   } catch (error) {
     next(error)
   }
-}
-
-// generate a unique room code
-async function generateRoomCode() {
-  // get a random string
-  let proposedCode = makeRandomCode()
-  let codeAlreadyUsed = await Room.findOne({ roomCode: proposedCode }).exec()
-
-  // keep getting strings until we find one that isn't assigned to a room already
-  while (codeAlreadyUsed) {
-    proposedCode = makeRandomString()
-    codeAlreadyUsed = await Room.findOne({ roomCode: proposedCode }).exec()
-  }
-
-  const uniqueCode = proposedCode
-  return uniqueCode
-}
-
-// make a 6 character string to feed generateRoomCode
-function makeRandomCode() {
-  // TODO-SA it would be kinda tight to restrict output to prevent repeat characters
-
-  // excluding 1, 2, 8, and 0 for reduced ambiguity
-  const symbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ345679'
-
-  // pick 6 character from symbols
-  let randomCode = ''
-  for (let i = 0; i < 6; i++) {
-    const randomIndex = Math.floor(Math.random() * symbols.length)
-    randomCode += symbols[randomIndex]
-  }
-
-  return randomCode
 }
 
 module.exports = {
--- models/room.js
+++ models/room.js
@@ -16,7 +16,6 @@ const roomSchema = new Schema({
   },
   roomCode: {
     type: String,
-    required: true,
   },
   parentalControlsOn: {
     type: Boolean,
@@ -31,5 +30,49 @@ const roomSchema = new Schema({
     default: false,
   },
 })
+
+// new rooms need to get a roomCode
+roomSchema.pre('save', async function (next) {
+  if (!this.isNew) return
+
+  this.roomCode = await generateRoomCode(this)
+  next()
+})
+
+// generate a unique room code
+async function generateRoomCode(document) {
+  const Room = document.constructor
+
+  // get a random string
+  let proposedCode = makeRandomCode()
+  let codeAlreadyUsed = await Room.findOne({ roomCode: proposedCode }).exec()
+
+  // keep getting strings until we find one that isn't assigned to a room already
+  // this kind of looping and random generation will get really slow if the possibility space starts to fill up
+  while (codeAlreadyUsed) {
+    proposedCode = makeRandomCode()
+    codeAlreadyUsed = await Room.findOne({ roomCode: proposedCode }).exec()
+  }
+
+  const uniqueCode = proposedCode
+  return uniqueCode
+}
+
+// make a 6 character string to feed generateRoomCode
+function makeRandomCode() {
+  // TODO-SA it would be kinda tight to restrict output to prevent repeat characters
+
+  // excluding 1, 2, 8, and 0 for reduced ambiguity
+  const symbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ345679'
+
+  // pick 6 character from symbols
+  let randomCode = ''
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * symbols.length)
+    randomCode += symbols[randomIndex]
+  }
+
+  return randomCode
+}
 
 module.exports = mongoose.model('Room', roomSchema)
