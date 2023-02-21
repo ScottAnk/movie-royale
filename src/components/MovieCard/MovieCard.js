68d3b541cab8c4ed71a5bf7eaa641e65061c45ca
eddie-hernandez <71785228+eddie-hernandez@users.noreply.github.com>
2023-02-21 15:24:16 -0500

    adding animations, displaying winning movie on screen
    Co-Authored-By: Paul Seabrook <43609896+paulseabrook@users.noreply.github.com>

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -12,23 +12,9 @@ export default function MovieCard({ movie, room, setRoom, isRecommended }) {
         <h4>
           <i>"{movie.title}"</i>
         </h4>
-      <li
-        className="MoviePoster"
-        style={{
-          backgroundImage: isRecommended
-            ? `radial-gradient(red, yellow)`
-            : `url(${movie.image})`,
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: '100% 100%',
-          height: '100%',
-          width: '100%',
-          minHeight: '200px',
-          minWidth: '100px',
-          maxHeight: '200px',
-          maxWidth: 'auto',
-        }}
-      ></li>
+      <li>
+        <img src={`${movie.image}`} className={isRecommended ? 'RotatedMoviePoster' : 'MoviePoster'} />
+      </li>
       <button
         disabled={isRecommended}
         onClick={handleRecommend}
--- src/pages/VotingRoom/VotingRoom.js
+++ src/pages/VotingRoom/VotingRoom.js
@@ -1,20 +1,41 @@
+import { useState } from 'react'
 import './VotingRoom.css'
 import RecommendedMovie from '../../components/RecommendedMovie/RecommendedMovie'
 
-
 export default function VotingRoom({ room, setRoom }) {
+  const [winner, SetWinner] = useState({}) 
+
+  function getWinningMovie() {
+    const highestNumber = 0
+
+    room.recommendedMovies.forEach((movie) => {
+      if (movie.usersVotingYes.length > highestNumber) {
+      SetWinner(movie)
+    }})
+    console.log(winner)
+  }
+
   return (
-    <div className="VotingRoomPageContainer">
-      <div>
-        <h2>It's time to vote on the movie you wanna see!</h2>
-      </div>
-      <ul className="RecommendedMovieContainer">
-        <div className="RecommendedMoviesGrid">
-        {room.recommendedMovies.map((movie, index) => (
-          <RecommendedMovie room={room} setRoom={setRoom} movie={movie} key={index} />
-        ))}
+    <div className="RoomPageContainer">
+      <div className="RoomCardContainer">
+      <div className="SectionContainer">
+        <h2 className="PageTitle">It's Votin' Time!</h2>
+        </div>
+      <ul className="RecdMovieGrid">
+        <div className="RecdMoviesContainer">
+          {room.recommendedMovies.map((movie, index) => (
+            <RecommendedMovie
+              room={room}
+              setRoom={setRoom}
+              movie={movie}
+              key={index}
+              winner={winner}
+              getWinningMovie={getWinningMovie}
+            />
+          ))}
         </div>
       </ul>
+      </div>
     </div>
   )
 }
