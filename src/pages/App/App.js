b4557a43c6776838e1743ae5e00b4df4252bc4fd
Scott Ankiewicz <scottfa.a@gmail.com>
2023-02-21 14:15:30 -0500

    integrate basic socket with react

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
+import io from 'socket.io-client'
+
 import { getUser } from '../../utilities/users-service'
 import AuthPage from '../AuthPage/AuthPage'
 import NavBar from '../../components/NavBar/NavBar'
@@ -11,10 +13,36 @@ import * as moviesAPI from '../../utilities/movies-api'
 import { findRoom } from '../../utilities/rooms-services'
 import './App.css'
 
+const socket = io()
+
 export default function App() {
   const [user, setUser] = useState(getUser())
   const [room, setRoom] = useState(findRoom())
   const [movies, setMovies] = useState([])
+
+  // boilerplate initialization from socket.io website - including useEffect below
+  const [isConnected, setIsConnected] = useState(socket.connected)
+
+  // set up socket events
+  useEffect(() => {
+    socket.on('connect', () => {
+      setIsConnected(true)
+    })
+
+    socket.on('disconnect', () => {
+      setIsConnected(false)
+    })
+
+    socket.on('test', (msg) => {
+      console.log(msg)
+    })
+
+    return () => {
+      socket.off('connect')
+      socket.off('disconnect')
+      socket.off('test')
+    }
+  }, [])
 
   useEffect(function () {
     async function getMovies() {
