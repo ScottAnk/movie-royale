c32570dc9341e63e0fbb825bab5f9a9e3a87efe2
Scott Ankiewicz <scottfa.a@gmail.com>
2023-04-22 16:31:29 -0400

    configure backend server address in environment

--- src/utilities/movies-api.js
+++ src/utilities/movies-api.js
@@ -1,6 +1,10 @@
 import sendRequest from './send-request'
 
-const BASE_URL = 'https://movie-royale.onrender.com/api/movies'
+const BASE_URL_PREFIX =
+  process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_DEPLOYMENT_BACKEND
+    : process.env.REACT_APP_DEVELOPMENT_BACKEND
+const BASE_URL = BASE_URL_PREFIX + '/api/movies'
 
 export async function getMovies() {
   return sendRequest(BASE_URL)
